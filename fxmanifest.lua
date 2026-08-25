de481ac1cc696f1acd851a258ec456e7f375f8a4
rico <r03292760@gmail.com>
2026-08-25 11:03:55 +0200

    Add resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Rico Scripts'
+description 'Scant resources en installeert ontbrekende items in ox_inventory'
+version '1.0.0'
+
+server_only 'yes'
+
+server_scripts {
+    'config.lua',
+    'server/main.lua'
+}
+
+files {
+    'data/report.json'
+}
+
+provide 'rs_itemmanager'
+

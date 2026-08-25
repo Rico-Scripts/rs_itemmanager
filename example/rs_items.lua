cf68cb481d04edad578d034ef1e98ac9189559b0
rico <r03292760@gmail.com>
2026-08-25 11:04:13 +0200

    Add item definition example

---
+++ example/rs_items.lua
@@ -0,0 +1,10 @@
+return {
+    ['voorbeeld_item'] = {
+        label = 'Voorbeelditem',
+        weight = 250,
+        stack = true,
+        close = true,
+        description = 'Voorbeeld voor rs_itemmanager'
+    }
+}
+

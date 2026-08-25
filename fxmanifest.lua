1bc1901c03ba50f1f01d52122ab636c2460ab86a
rico <r03292760@gmail.com>
2026-08-25 14:03:43 +0200

    Release v1.2.0 with safe cleanup

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,17 +3,22 @@ game 'gta5'
 
 author 'Rico Scripts'
 description 'Scant resources en installeert ontbrekende items in ox_inventory'
-version '1.1.1'
+version '1.2.0'
 
 server_only 'yes'
 
 server_scripts {
+    '@oxmysql/lib/MySQL.lua',
     'config.lua',
-    'server/main.lua'
+    'server/main.lua',
+    'server/cleanup.lua'
 }
 
 files {
-    'data/report.json'
+    'data/report.json',
+    'data/cleanup-report.json'
 }
+
+dependency 'oxmysql'
 
 provide 'rs_itemmanager'
